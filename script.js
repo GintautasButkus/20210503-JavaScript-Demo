104ad5a1054452191ba09b3be1a2ba7be65f1fbf
GintautasButkus <82833055+GintautasButkus@users.noreply.github.com>
2021-05-03 20:07:05 +0300

    Initial JS commit

---
+++ script.js
@@ -0,0 +1,35 @@
+// let number = 10;
+// console.log(number);
+
+// let stringtype = "String"
+// let boolValue = false
+
+// console.log(stringtype);
+
+// let numberArray = [20, 10, 30]
+// console.log(numberArray);
+
+// let numberString = prompt("Please enter a number");
+// let number = parseInt(numberString)
+
+// if (number == 10){
+//     console.log("Number is 10");
+// }
+// else if (number == 20){
+//     console.log("number is 10*2");
+// }
+// else {
+//     console.log("number is not 10");
+// }
+
+let array = [20, 10, 30, 70];
+
+let sum = 0;
+
+for (let i = 0; i < array.length; i++) 
+{const element = array[i];
+    sum = sum + element;}
+
+console.log(sum);
+
+
