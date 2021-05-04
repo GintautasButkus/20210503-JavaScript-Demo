09f3b4486e870bbec54c88997cdb8b94d153a5bf
GintautasButkus <82833055+GintautasButkus@users.noreply.github.com>
2021-05-04 20:00:37 +0300

    4May Commit

--- script.js
+++ script.js
@@ -22,14 +22,45 @@
 //     console.log("number is not 10");
 // }
 
-let array = [20, 10, 30, 70];
+// let Array = [20, 10, 30, 70];
 
-let sum = 0;
+// let sum = 0;
 
-for (let i = 0; i < array.length; i++) 
-{const element = array[i];
-    sum = sum + element;}
+// for (let i = 0; i < array.length; i++)
+// {const element = array[i];
+//     sum = sum + element;}
 
-console.log(sum);
+// console.log(sum);
 
+// 4MAY2021
+
+function multiply(p1, p2) {
+  return p1 * p2; // The function returns the product of p1 and p2
+}
+console.log(multiply(3, 3));
+// --------------------------------------
+function divide(p1, p2) {
+  return p1 / p2;
+}
+console.log(divide(6, 3));
+
+//   --------------------------
+function multiply(p1, p2) {
+  let multivalue = p1 * p2;
+  return multivalue;
+}
+console.log(multiply(1, 2));
+
+// --------------------------------------
+function factorial(x) 
+{ 
+
+  if (x === 0)
+ {
+    return 1;
+ }
+  return x * factorial(x-1);
+         
+}
+console.log(factorial(5));
 
